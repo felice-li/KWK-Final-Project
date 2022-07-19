a65b578c9735adf0d350f9e72f757b610b3b35d5
Felice Li <feliceli010@gmail.com>
2022-07-19 14:53:21 -0400

    Initial Commit

---
+++ KWK Final ProjectTests/KWK_Final_ProjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  KWK_Final_ProjectTests.swift
+//  KWK Final ProjectTests
+//
+//  Created by Felice Li on 7/19/22.
+//
+
+import XCTest
+@testable import KWK_Final_Project
+
+class KWK_Final_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
